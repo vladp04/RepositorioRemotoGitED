cf4f7a006048af455e3f77e2f388df90a0303c14
iesro <iesro@DAM1-08>
2026-01-15 09:45:35 +0100

    Commit inicial: Clase Principal con un mensaje

---
+++ ProyectoGitED/src/PaqueteGitED/ClaseGitED.java
@@ -0,0 +1,7 @@
+package PaqueteGitED;
+
+public class ClaseGitED {
+	public static void main(String[] args) {
+		System.out.println("Mensaje inicial desde rama main");
+	}
+}
---
+++ ProyectoGitED/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ProyectoGitED {
+}
