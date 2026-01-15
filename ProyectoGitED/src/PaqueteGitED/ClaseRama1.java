f855e58a4ba3462ca47c76f561fec2da719970a8
iesro <iesro@DAM1-08>
2026-01-15 10:31:22 +0100

    Añadidos mensajes 3 y 4 en rama1

---
+++ ProyectoGitED/src/PaqueteGitED/ClaseRama1.java
@@ -0,0 +1,10 @@
+package PaqueteGitED;
+
+public class ClaseRama1 {
+	public static void main(String[] args) {
+		System.out.println("Mensaje 1 desde rama1");
+		System.out.println("Mensaje 2 desde rama1");
+		System.out.println("Mensaje 3 añadido después");
+        System.out.println("Mensaje 4 añadido después");
+	}
+}
